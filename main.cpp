1e1a35786562bf65427fbe85dc2d56ef47dd77f5
Adara Carrillo <acarrillo49@csub.edu>
2021-02-02 11:09:59 -0800

    lab1 changes

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 // CMPS 3350 - lab 1
 // Prints each command-line argument passed into the program, one per line
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char ** argv)
 {
-    for(int i = 1; i < argc; i++) {
+    for(int i = 1; i < argc; i++)
+    {
         printf("arg %d: %s\n", i, argv[i]);
     }
+
+    cout << "testing... 1, 2, 3...." << endl;
     return 0;
 }
 
